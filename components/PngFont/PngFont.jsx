3443555c93e52c55c5014bf398942c066d981624
Heitor José Tessaro <85108199+htessaro@users.noreply.github.com>
2025-04-17 10:23:33 -0300

    Update PngFont.jsx

--- components/PngFont/PngFont.jsx
+++ components/PngFont/PngFont.jsx
@@ -8,6 +8,7 @@ export default function PngFont({ name }) {
         src={useBaseUrl(`media/designer/icons/${name}.png`)}
         alt={`${name} icon`}
         style={{ display: "inline", height: "18px", width: "18px" }}
+        className="no_zoom"
       />
     </span>
   );
